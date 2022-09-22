6f1c590c36b198bd13a23782e11a71920461d00b
uluizeduardo <luiz.analistadesistemas@gmail.com>
2022-09-21 21:51:57 -0300

    Finalizado a validação do CPF

--- doguito/assets/js/validacao.js
+++ doguito/assets/js/validacao.js
@@ -78,7 +78,7 @@ function validaCPF(input){
     const cpfFormatado = input.value.replace(/\D/g, '');   
     let mensagem = '';
 
-    if(!checaCPFRepetido(cpfFormatado)){
+    if(!checaCPFRepetido(cpfFormatado) || !checaEstruturaCPF(cpfFormatado)){
         mensagem = 'O CPF digitado não é válido.'
     }
     input.setCustomValidity(mensagem);
@@ -101,10 +101,43 @@ function checaCPFRepetido(cpf){
     let cpfValido = true;
 
     valoresRepetidos.forEach(valor => {
-        if(valor === cpf){
+        if(valor == cpf){
             cpfValido = false;
         }
     })
 
     return cpfValido;
+}
+
+function checaEstruturaCPF(cpf){
+    const multiplicador = 10;
+
+    return checaDigitoVerificador(cpf, multiplicador);
+
+}
+
+function checaDigitoVerificador(cpf, multiplicador){
+
+    if(multiplicador >= 12){
+        return true;
+    }
+
+    let multiplicadorInicial = multiplicador;
+    let soma = 0;
+    const cpfSemDigitos = cpf.substr(0, multiplicador - 1).split('');
+    const digitoVerificador = cpf.charAt(multiplicador - 1);
+
+    for (let contador = 0; multiplicadorInicial > 1; multiplicadorInicial--) {
+        soma = soma + cpfSemDigitos[contador] * multiplicadorInicial
+        contador++        
+    }
+
+    if(digitoVerificador == confirmaDigito(soma)){
+        return checaDigitoVerificador(cpf, multiplicador + 1);
+    }
+    return false;
+}
+
+function confirmaDigito(soma){
+    return 11 - (soma % 11);
 }
